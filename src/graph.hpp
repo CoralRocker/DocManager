a717a7864ebd9946b9e5cb477750d020a3c41181
Gaultier Delbarre <gaultiertexas@gmail.com>
2022-09-29 13:16:56 -0500

    Added BFS/DFS iterator

--- src/graph.hpp
+++ src/graph.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstddef>
 #include <filesystem>
 #include <iterator>
 #include <memory>
@@ -9,6 +10,7 @@
 #include <stack>
 #include <iterator>
 #include <type_traits>
+#include <set>
 
 #include "document.hpp"
 #include "utils.hpp"
@@ -31,9 +33,10 @@ class docgraph {
       using iterator_category = std::forward_iterator_tag;
       using value_type = shared_ptr<document>;
       using Container = std::conditional_t<TYPE == BFS, std::queue<value_type>, std::stack<value_type>>;
-      using reference = std::conditional_t<Const, const value_type&, value_type&>;
-      using pointer = std::conditional_t<Const, const value_type*, value_type*>;
+      using reference = std::conditional_t<Const, const document&, document&>;
+      using pointer = std::conditional_t<Const, const value_type, value_type>;
       private:
+      std::set<value_type> visited;
       Container cont;
       shared_ptr<document> cur;
 
@@ -54,18 +57,97 @@ class docgraph {
       }
 
       public:
-      explicit _iterator(value_type root) {
+
+      explicit _iterator(value_type root) : cur(root) {
         if( !root.get() )
           return;
-        if( !root->references.empty() ){
-          cont.insert(cont.begin(), root->references.begin(), root->references.end());
+        
+        visited.insert(root);
+
+        for( auto ref : root->references ){
+          if( visited.find(ref) == visited.end() ){
+            cont.push(ref);
+          }
         }
+        //if( !root->references.empty() ){
+        //  cont.insert(cont.begin(), root->references.begin(), root->references.end());
+        //}
       }
 
+      _iterator() = delete;
+      _iterator(const _iterator&) = default;
+      _iterator(const _iterator&& rhs) : visited(std::move(rhs.visited)), cont(std::move(rhs.cont)), cur(std::move(rhs.cur)) {}
+      _iterator& operator=(const _iterator&) = default;
+      _iterator& operator=(const _iterator&& rhs) {
+        visited = std::move(rhs.visited);
+        cont = std::move(rhs.cont);
+        cur = std::move(rhs.cur);
+        return *this;
+      }
+
+      template<bool wasConst, class = std::enable_if<Const && ! wasConst>>
+      _iterator(const _iterator<wasConst, TYPE> &rhs) : cont(rhs.cont), cur(rhs.cur) {}
+
+      template<bool wasConst, class = std::enable_if<Const && ! wasConst>>
+      _iterator(const _iterator<wasConst, TYPE> &&rhs) : cont(std::move(rhs.cont)), cur(std::move(rhs.cur)) {}
+
+      template<bool wasConst, class =  std::enable_if<Const && !wasConst>>
+      _iterator& operator=(const _iterator<wasConst, TYPE>& rhs) {
+        visited = rhs.visited;
+        cont = rhs.cont;
+        cur = rhs.cur;
+        return *this;
+      }
+      
+      template<bool wasConst, class =  std::enable_if<Const && !wasConst>>
+      _iterator& operator=(const _iterator<wasConst, TYPE>&& rhs) {
+        visited = std::move(rhs.visited);
+        cont = std::move(rhs.cont);
+        cur = std::move(rhs.cur);
+        return *this;
+      }
+
+      friend class _iterator<!Const, TYPE>;
+
+      pointer operator->() { return cur; }
+      reference operator*() { return *cur; }
+
+      bool operator==(const _iterator &other) const { return cur.get() == other.cur.get(); }
+      bool operator!=(const _iterator &other) const { return cur.get() != other.cur.get(); }
+
+      _iterator& operator++() {
+        if( cont.empty() ) { cur = nullptr; return *this; }
+
+        cur = pop();
+
+        for( auto ref : cur->references ) {
+          if( visited.find(ref) == visited.end() )
+            cont.push(ref);
+        }
+        return *this;
+      }
+
+      _iterator operator++(int) {
+        _iterator result(*this);
+        if( cont.empty() ) { cur = nullptr; return result; }
+        
+        cur = pop();
+
+        for( auto ref : cur->references )
+          if( visited.find(ref) == visited.end() )
+            cont.push(ref);
+        return result;
+      }
 
 
     };
   public:
+    using DFSiterator = _iterator<false, DFS>;
+    using constDFSiterator = _iterator<true, DFS>;
+    using BFSiterator = _iterator<false, BFS>;
+    using constBFSiterator = _iterator<true, BFS>;
+
+
     docgraph() = default;
 
     void scan_dir(path);
@@ -94,9 +176,16 @@ class docgraph {
     decltype(docs)::const_reverse_iterator crend() { return docs.crend(); }
 
     // DFS And BFS Iterators
-    // TODO
+    DFSiterator dfsbegin(size_t idx) { return DFSiterator(docs.at(idx)); }
+    DFSiterator dfsend() { return DFSiterator(nullptr); }
+    constDFSiterator cdfsbegin(size_t idx) const { return constDFSiterator(docs.at(idx)); }
+    constDFSiterator cdfsend() const { return constDFSiterator(nullptr); }
 
-
+    BFSiterator bfsbegin(size_t idx) { return BFSiterator(docs.at(idx)); }
+    BFSiterator bfsend() { return BFSiterator(nullptr); }
+    constBFSiterator cbfsbegin(size_t idx) const { return constBFSiterator(docs.at(idx)); }
+    constBFSiterator cbfsend() const { return constBFSiterator(nullptr); }
+    
     vector<shared_ptr<document>> getDoc(string, decltype(string::npos)=3); 
 
     size_t size() const {
